060b1a25d4518357dea73875046b8adf66c9f245
root <root@root.com>
2021-04-21 01:26:10 +0000

    finish up random == 3

--- _03_print_and_popups/_d_quiz_game.py
+++ _03_print_and_popups/_d_quiz_game.py
@@ -1,24 +1,33 @@
 from tkinter import messagebox, simpledialog, Tk
 
 # Create an if-main code block, *hint, type main then ctrl+space to auto-complete
-    
-    # Make a new window variable, window = Tk()
-    
-    # Hide the window using the window's .withdraw() method
-    
-    # 1. Create a variable to hold the user's score. Set it equal to zero. 
+if __name__ == '__main__':
 
+    # Make a new window variable, window = Tk()
+    window=Tk()
+    # Hide the window using the window's .withdraw() method
+    window.withdraw()
+    # 1. Create a variable to hold the user's score. Set it equal to zero. 
+    score=0
     # ASK A QUESTION AND CHECK THE ANSWER
 
     #      // 2.  Ask the user a question 
-    
+    question1=simpledialog.askstring(title='Question 1', prompt='What is 2+2?')
     #      // 3.  Use an if statement to check if their answer is correct
-
+    if question1 == '4':
+        score=score+1
     #      // 4.  if the user's answer was correct, add one to their score 
- 
+    else:
+        score=score-1
     # MAKE MORE QUESTIONS. Ask more questions by repeating the above 
     #      // Option: Subtract a point from their score for a wrong answer
- 
+    question2=simpledialog.askstring(title='Question 2', prompt= 'What is 4+4?')
+    if question2 == '8':
+        score=score+1
+    else:
+        score=score-1
+    str(score)
+    messagebox.showinfo(title='Score', message='Your score is ' +str(score))
     # After all the questions have been asked, tell the user their final score
     # remember to convert your variable to a string using the str() function 
     
